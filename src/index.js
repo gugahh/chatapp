4472f46a72ce9f27a72a4e5278c3a6cf67e88cf1
Gustavo Santos <gugahh.br@gmail.com>
2020-09-17 01:30:31 -0300

    Add randomizador de resultados (Faker)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
 
-    const pessoaIcon = "https://icon-icons.com/icons2/806/PNG/48/chat-53_icon-icons.com_65955.png";
+    // const pessoaIcon = "https://icon-icons.com/icons2/806/PNG/48/chat-53_icon-icons.com_65955.png";
 
 	return (
 		<div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img src={pessoaIcon} alt="avatar" />
+                    <img src={faker.image.avatar()} alt="avatar" />
                 </a>
             </div>
             <div className="comment">
                 <a href="/" className="author">
-                    Sissy Spacek
+                    {faker.name.firstName()} {faker.name.lastName()}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6PM</span>
+                    <span className="date">{faker.date.recent().toLocaleString()}</span>
                 </div>
             </div>
             <div className="text">
-                I like it very much. Keep up the good work!
+                {faker.hacker.phrase()}
             </div>
         </div>
 	);
